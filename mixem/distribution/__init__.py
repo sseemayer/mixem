f1ddb456a7ccf4699ce89811bcab4425aa771ba5
Stefan Seemayer <stefan@seemayer.de>
2016-01-25 13:20:56 +0100

    Remove import to simpledistribution

--- mixem/distribution/__init__.py
+++ mixem/distribution/__init__.py
@@ -1,5 +1,4 @@
 from mixem.distribution.distribution import Distribution
-from mixem.distribution.simple import SimpleDistribution
 from mixem.distribution.normal import NormalDistribution, MultivariateNormalDistribution
 from mixem.distribution.exponential import ExponentialDistribution
 
