7f54c7676583f82bd893e1ba00ac72d6302436a1
Stefan Seemayer <stefan@seemayer.de>
2016-01-28 09:43:32 +0100

    Simplify mixem.probability: Allow passing single values

--- mixem/model.py
+++ mixem/model.py
@@ -5,6 +5,10 @@ def probability(data, weights, distributions):
     """Compute the probability for data of the mixture density model given by weights and a list of distributions"""
 
     assert len(weights) == len(distributions), "Need matching number of weights and distributions!"
+
+    if not hasattr(data, '__len__'):
+        data = [data]
+
     data = np.array(data)
 
     n_dist = len(distributions)
